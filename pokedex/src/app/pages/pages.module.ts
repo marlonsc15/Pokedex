e01b2736cde50269423b0e778ccabf5576de71bd
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-09-09 17:09:51 -0300

    criação dos componentes header/details/search

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -1,12 +1,21 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './roating.module';
+import { DetailsComponent } from './details/details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
